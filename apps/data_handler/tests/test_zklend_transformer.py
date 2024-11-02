1acf6bde0968419fabcf70e4df6df89ab7a78c85
Ikem Peter <ikempeter2020@gmail.com>
2024-11-02 08:23:09 +0100

    fixed pylint

--- apps/data_handler/tests/test_zklend_transformer.py
+++ apps/data_handler/tests/test_zklend_transformer.py
@@ -54,7 +54,12 @@ def sample_borrowing_event_data() -> Dict[str, Any]:
         "event_index": 3,
         "from_address": "0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05",
         "keys": ["0xfa3f9acdb7b24dcf6d40d77ff2f87a87bca64a830a2169aebc9173db23ff41"],
-        "data": ["0x1a0027d1bf86904d1051fe0ca94c39b659135f19504d663d66771a7424ca2eb", "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7", "0x8ec920c39e9", "0x9184e72a000"],
+        "data": [
+            "0x1a0027d1bf86904d1051fe0ca94c39b659135f19504d663d66771a7424ca2eb",
+            "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+            "0x8ec920c39e9",
+            "0x9184e72a000"
+        ],
         "timestamp": 1712276824,
         "key_name": "zklend::market::Market::Borrowing"
     }
@@ -73,7 +78,13 @@ def sample_repayment_event_data() -> Dict[str, Any]:
         "event_index": 5,
         "from_address": "0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05",
         "keys": ["0x7ae0ab7952bbfc33a72035e5eccec7c8816723421c0acb315bd4690a71d46e"],
-        "data": ["0x7f121e44b3f446cdcaa28b230546956208d51e96894acc3b482947356bc10ed", "0x7f121e44b3f446cdcaa28b230546956208d51e96894acc3b482947356bc10ed", "0x3fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac", "0xd60", "0xdaf"],
+        "data": [
+            "0x7f121e44b3f446cdcaa28b230546956208d51e96894acc3b482947356bc10ed",
+            "0x7f121e44b3f446cdcaa28b230546956208d51e96894acc3b482947356bc10ed",
+            "0x3fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac",
+            "0xd60",
+            "0xdaf"
+        ],
         "timestamp": 1712275350,
         "key_name": "zklend::market::Market::Repayment"
     }
@@ -92,7 +103,11 @@ def sample_deposit_event_data() -> Dict[str, Any]:
         "event_index": 5,
         "from_address": "0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05",
         "keys": ["0x9149d2123147c5f43d258257fef0b7b969db78269369ebcf5ebb9eef8592f2"],
-        "data": ["0x1cfa080d4bbddc206637afad05e5e1abb04da69630f40b2fd9dc578e618ec78", "0x585c32b625999e6e5e78645ff8df7a9001cf5cf3eb6b80ccdd16cb64bd3a34", "0x14839256fce60ba2c"],
+        "data": [
+            "0x1cfa080d4bbddc206637afad05e5e1abb04da69630f40b2fd9dc578e618ec78",
+            "0x585c32b625999e6e5e78645ff8df7a9001cf5cf3eb6b80ccdd16cb64bd3a34",
+            "0x14839256fce60ba2c"
+        ],
         "timestamp": 1712276824,
         "key_name": "zklend::market::Market::Deposit"
     }
@@ -111,7 +126,11 @@ def sample_withdrawal_event_data() -> Dict[str, Any]:
         "event_index": 3,
         "from_address": "0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05",
         "keys": ["0x2eed7e29b3502a726faf503ac4316b7101f3da813654e8df02c13449e03da8"],
-        "data": ["0x49920f8f551060f726e3c8c2fe26e2b39376027450b7672634824bd60c64022", "0x585c32b625999e6e5e78645ff8df7a9001cf5cf3eb6b80ccdd16cb64bd3a34", "0x176b344f2a78c0000"],
+        "data": [
+            "0x49920f8f551060f726e3c8c2fe26e2b39376027450b7672634824bd60c64022",
+            "0x585c32b625999e6e5e78645ff8df7a9001cf5cf3eb6b80ccdd16cb64bd3a34",
+            "0x176b344f2a78c0000"
+        ],
         "timestamp": 1712277188,
         "key_name": "zklend::market::Market::Withdrawal"
     }
@@ -131,7 +150,10 @@ def sample_collateral_enabled_event_data() -> Dict[str, Any]:
         "event_index": 0,
         "from_address": "0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05",
         "keys": ["0x2324062bde6ebb76ffd17d55fee62fee62a4877588eb02524b19c091983b365"],
-        "data": ["0x54f9574d3029b81e0d64e3267a7b682ab920b57948644de4581f5ceb30351ea", "0xda114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3"],
+        "data": [
+            "0x54f9574d3029b81e0d64e3267a7b682ab920b57948644de4581f5ceb30351ea",
+            "0xda114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3"
+        ],
         "timestamp": 1712277561,
         "key_name": "zklend::market::Market::CollateralEnabled"
     }
@@ -150,7 +172,10 @@ def sample_collateral_disabled_event_data() -> Dict[str, Any]:
         "event_index": 0,
         "from_address": "0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05",
         "keys": ["0xf999d0d33513a8215756ae6a9223180a439c134372b158bc84b9dd02f63856"],
-        "data": ["0x237be5917e0f4ceba3067af8f11137cc7262b9444fce220183e7fdffe4c1a40", "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"],
+        "data": [
+            "0x237be5917e0f4ceba3067af8f11137cc7262b9444fce220183e7fdffe4c1a40",
+            "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
+        ],
         "timestamp": 1712284226,
         "key_name": "zklend::market::Market::CollateralDisabled"
     }
