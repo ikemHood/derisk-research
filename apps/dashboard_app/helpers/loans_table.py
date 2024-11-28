d15dac987173c3232a5afda3ee9c5c5935e140c8
Ikem Peter <ikempeter2020@gmail.com>
2024-11-28 05:25:17 +0100

    updated classes that extends States

--- apps/dashboard_app/helpers/loans_table.py
+++ apps/dashboard_app/helpers/loans_table.py
@@ -7,16 +7,7 @@ from shared.types import Prices
 
 
 def get_protocol(state: State) -> str:
-    # TODO: Improve the inference.
-    if isinstance(state, ZkLendState):
-        return "zkLend"
-    if isinstance(state, NostraAlphaState) and not isinstance(
-        state, NostraMainnetState
-    ):
-        return "Nostra Alpha"
-    if isinstance(state, NostraMainnetState):
-        return "Nostra Mainnet"
-    raise ValueError
+    return state.get_protocol_name
 
 
 def get_loans_table_data(
